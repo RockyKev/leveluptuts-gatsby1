1040cbb336b4234fcfdfbbd8a1d47ae25f311230
Rocky Kev <kev.rocky@gmail.com>
2020-01-03 12:28:39 -0800

    dynamically creating pages in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,37 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+//path is part of node, so no need to install it
+const path = require("path");
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  //Destructuring --> same as actions.createPage
+
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then(results => {
+      results.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.frontmatter.slug,
+          component: path.resolve("./src/components/postLayout.js"),
+          context: {
+            slug: node.formatter.slug,
+          },
+        });
+      });
+    });
+    resolve();
+  });
+};
