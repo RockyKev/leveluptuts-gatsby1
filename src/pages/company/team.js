9998e3fb5ab618ad6a84be1949899d22d1d37b6b
Rocky Kev <kev.rocky@gmail.com>
2020-01-02 23:07:38 -0800

    added company and about

---
+++ src/pages/company/team.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Layout from '../../components/layout'
+import Image from '../../components/image'
+import SEO from '../../components/seo'
+
+const Team = () => (
+  <Layout>
+    <div>
+      <h1> The Team </h1>
+      <p>
+        Bacon ipsum dolor amet beef pork strip steak jerky cow bresaola.
+        Landjaeger bresaola capicola, turducken shoulder turkey ball tip ham
+        tongue jerky sirloin rump short loin. Bacon ham hock bresaola picanha
+        meatloaf. Pastrami chicken kevin prosciutto meatloaf. Shankle jerky ball
+        tip pancetta. T-bone swine doner chuck ground round, brisket boudin beef
+        ribs alcatra tenderloin rump kevin shoulder kielbasa.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default Team
