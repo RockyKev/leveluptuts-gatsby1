6d86ef7437af5f444110024e62fe3f9471735147
Rocky Kev <kev.rocky@gmail.com>
2020-01-03 12:00:36 -0800

    updated

--- src/components/archive.js
+++ src/components/archive.js
@@ -1,33 +1,45 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
-const Archive = () => {
-  const data = useStaticQuery(graphql`
-    query BlogPostArchive {
-      allMarkdownRemark {
-        edges {
-          node {
-            wordCount {
-              words
-            }
-            frontmatter {
-              title
-              slug
-            }
+const POST_ARCHIVE_QUERY = graphql`
+  query BlogPostArchive {
+    allMarkdownRemark(
+      limit: 5
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          wordCount {
+            words
+          }
+          frontmatter {
+            title
+            slug
           }
         }
       }
     }
-  `)
+  }
+`
+
+const Archive = () => {
+  const data = useStaticQuery(POST_ARCHIVE_QUERY)
 
   return (
     <>
       <aside>
         <h3>Archive</h3>
-        {data.allMarkdownRemark.edges.map(edge => (
-          <li> {edge.node.frontmatter.title} </li>
-        ))}
+        <ul>
+          {data.allMarkdownRemark.edges.map(edge => (
+            <li key={edge.node.frontmatter.slug}>
+              <Link to={`/posts/${edge.node.frontmatter.slug}`}>
+                {" "}
+                {edge.node.frontmatter.title}{" "}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </aside>
     </>
   )
